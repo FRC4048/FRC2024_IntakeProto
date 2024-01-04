a4a1e6f261dad3ffd69debeec3979cd4b0444f87
goatfanboi23 <dev.codetoad@gmail.com>
2024-01-03 19:38:52 -0500

    init commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.ToggleIntakeStates;
+import frc.robot.subsystems.Intake;
+import frc.robot.utils.Constants;
+
+public class RobotContainer {
+    private final CommandXboxController controller = new CommandXboxController(Constants.CONTROLLER_ID);
+    private final Intake intake;
+    public RobotContainer() {
+        this.intake = new Intake();
+        configureBindings();
+    }
+    private void configureBindings() {
+        controller.button(XboxController.Button.kA.value).onTrue(new ToggleIntakeStates(intake));
+    }
+
+}
