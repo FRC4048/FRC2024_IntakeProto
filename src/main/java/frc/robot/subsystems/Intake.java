986b0b692f419be697e82cc3f18c2c9890e7642f
Aditya Hebbar <Adityarhebbar@gmail.com>
2024-01-12 19:33:51 -0500

    swapped everything with Sparkmax

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,19 +1,22 @@
 package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.Constants;
 import frc.robot.utils.IntakeState; 
 
 public class Intake extends SubsystemBase {
-    private final PWMTalonSRX talonSRX1;
-    private final PWMTalonSRX talonSRX2;
+    private final CANSparkMax CANSparkMax1;
+    private final CANSparkMax CANSparkMax2;
     private IntakeState currentState = IntakeState.STOPPED;
 
     public Intake(){
-        this.talonSRX1 = new PWMTalonSRX(Constants.INTAKE_MOTOR1_ID);
-        this.talonSRX2 = new PWMTalonSRX(Constants.INTAKE_MOTOR2_ID);
+        this.CANSparkMax1 = new CANSparkMax(Constants.INTAKE_MOTOR1_ID, MotorType.kBrushless);
+        this.CANSparkMax2 = new CANSparkMax(Constants.INTAKE_MOTOR2_ID, MotorType.kBrushless);
     }
-
+    //Replace PWMTalonSRX w/sparkmaxes and 
     /**
      * sets the speed of both motors to the IntakeSpeed defined in the {@link Constants} file
      * @param forward if true the motors will spin in the intake directions,
@@ -21,13 +24,13 @@ public class Intake extends SubsystemBase {
      */
     public void spin(boolean forward){
         double speed = forward ? Constants.INTAKE_SPEED : Constants.INTAKE_SPEED*-1;
-        talonSRX1.set(speed);
-        talonSRX2.set(-speed);
+        CANSparkMax1.set(speed);
+        CANSparkMax2.set(-speed);
         currentState = forward ? IntakeState.FORWARD: IntakeState.BACKWARD;
     }
     public void stop(){
-        talonSRX1.set(0);
-        talonSRX2.set(0);
+        CANSparkMax1.set(0);
+        CANSparkMax2.set(0);
         currentState = IntakeState.STOPPED;
     }
 
