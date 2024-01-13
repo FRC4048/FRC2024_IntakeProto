89327d49f85e5feeaf6ef775cabf5c83a3455393
Aditya Hebbar <Adityarhebbar@gmail.com>
2024-01-12 19:44:31 -0500

    made a few minor changes

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.Constants;
 import frc.robot.utils.IntakeState; 
@@ -10,22 +10,30 @@ import frc.robot.utils.IntakeState;
 public class Intake extends SubsystemBase {
     private final CANSparkMax CANSparkMax1;
     private final CANSparkMax CANSparkMax2;
+    //private final CANSparkMax CANSparkMax3;
+    //private final CANSparkMax CANSparkMax4;
+    private double INTAKE_SPEED = 0.75;
     private IntakeState currentState = IntakeState.STOPPED;
 
     public Intake(){
         this.CANSparkMax1 = new CANSparkMax(Constants.INTAKE_MOTOR1_ID, MotorType.kBrushless);
         this.CANSparkMax2 = new CANSparkMax(Constants.INTAKE_MOTOR2_ID, MotorType.kBrushless);
+        //this.CANSparkMax3 = new CANSparkMax(Constants.INTAKE_MOTOR1_ID, MotorType.kBrushless);
+        //this.CANSparkMax4 = new CANSparkMax(Constants.INTAKE_MOTOR2_ID, MotorType.kBrushless);
+        SmartDashboard.putNumber("Intake Motor Speeds", 0.75);
     }
-    //Replace PWMTalonSRX w/sparkmaxes and 
     /**
      * sets the speed of both motors to the IntakeSpeed defined in the {@link Constants} file
      * @param forward if true the motors will spin in the intake directions,
      *                if false the motors will spin in the outtake direction
      */
     public void spin(boolean forward){
-        double speed = forward ? Constants.INTAKE_SPEED : Constants.INTAKE_SPEED*-1;
+        INTAKE_SPEED = SmartDashboard.getNumber("Intake Motor Speeds", 0.75);
+        double speed = forward ? INTAKE_SPEED : INTAKE_SPEED*-1;
         CANSparkMax1.set(speed);
         CANSparkMax2.set(-speed);
+        //CANSparkMax3.set(-speed);
+        //CANSparkMax4.set(-speed);
         currentState = forward ? IntakeState.FORWARD: IntakeState.BACKWARD;
     }
     public void stop(){
