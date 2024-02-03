af7fea5ce09b6d1c0002db060141f7bd2d9a15b4
Aditya Hebbar <Adityarhebbar@gmail.com>
2024-02-02 20:20:58 -0500

    added botpose in meters

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -22,6 +22,7 @@ public class Limelight extends SubsystemBase {
   NetworkTableEntry ta;
   NetworkTableEntry thor;
   NetworkTableEntry tvert;
+  NetworkTableEntry botpose;
   double cameraHeight = 25; //inches
   double targetHeight = 50; //height to the bottom of the target in inches
   double cameraAngle = 15; //mounting angle
@@ -35,11 +36,18 @@ public class Limelight extends SubsystemBase {
     ta = table.getEntry("ta");
     thor = table.getEntry("thor");
     tvert = table.getEntry("tvert");
+    botpose = table.getEntry("targetpose_robotspace");
+    SmartDashboard.putNumber("ExpectedBotposeX", 0);
+    SmartDashboard.putNumber("ExpectedBotposeY", 0);
+    SmartDashboard.putNumber("ExpectedBotposeZ", 0);
   }
   @Override
   public void periodic() {
     double forwardDistance = (targetHeight - cameraHeight) / Math.tan(Math.toRadians(cameraAngle + ty.getNumber(0.0).doubleValue()));
     double sidewaysDistance = forwardDistance * Math.tan(Math.toRadians(tx.getNumber(0.0).doubleValue())); //Middle of the camera to the middle of the tag
+    Number[] botposeArray = {0.0, 0.0};
+    botposeArray = botpose.getNumberArray(botposeArray);
+    
     SmartDashboard.putNumber("tv", tv.getNumber(0).doubleValue());
     SmartDashboard.putNumber("tx", tx.getNumber(0.0).doubleValue());
     SmartDashboard.putNumber("ty", ty.getNumber(0.0).doubleValue());
@@ -48,5 +56,17 @@ public class Limelight extends SubsystemBase {
     SmartDashboard.putNumber("tvert", tvert.getNumber(0.0).doubleValue());
     SmartDashboard.putNumber("tforwardDistance", forwardDistance);
     SmartDashboard.putNumber("tsidewaysDistance", sidewaysDistance);
+    SmartDashboard.putNumber("botposeX", botposeArray[0].doubleValue());
+    SmartDashboard.putNumber("botposeY", botposeArray[1].doubleValue());
+    SmartDashboard.putNumber("botposeZ", botposeArray[2].doubleValue());
+    SmartDashboard.putNumber("botposeRoll", botposeArray[3].doubleValue());
+    SmartDashboard.putNumber("botposePitch", botposeArray[4].doubleValue());
+    SmartDashboard.putNumber("botposeYaw", botposeArray[5].doubleValue());
+    SmartDashboard.putNumber("DiferenceBotposeX", botposeArray[0].doubleValue() - SmartDashboard.getNumber("ExpectedBotposeZ", 0));
+    SmartDashboard.putNumber("DiferenceBotposeY", botposeArray[1].doubleValue() - SmartDashboard.getNumber("ExpectedBotposeY", 0));
+    SmartDashboard.putNumber("DiferenceBotposeZ", botposeArray[2].doubleValue() - SmartDashboard.getNumber("ExpectedBotposeZ", 0));
+    SmartDashboard.putNumber("MultiplierBotposeX",  botposeArray[0].doubleValue() / SmartDashboard.getNumber("ExpectedBotposeX", 0));
+    SmartDashboard.putNumber("MultiplierBotposeY", botposeArray[1].doubleValue() / SmartDashboard.getNumber("ExpectedBotposeY", 0));
+    SmartDashboard.putNumber("MultiplierBotposeZ", botposeArray[2].doubleValue() / SmartDashboard.getNumber("ExpectedBotposeZ", 0));
   }
 }
