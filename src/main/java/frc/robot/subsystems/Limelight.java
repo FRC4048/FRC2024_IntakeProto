20def088ebccc364364e616934a313c5f0560420
Aditya Hebbar <Adityarhebbar@gmail.com>
2024-02-04 11:44:15 -0500

    inches based :D

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -23,6 +23,7 @@ public class Limelight extends SubsystemBase {
   NetworkTableEntry thor;
   NetworkTableEntry tvert;
   NetworkTableEntry botpose;
+  NetworkTableEntry tid;
   double cameraHeight = 25; //inches
   double targetHeight = 50; //height to the bottom of the target in inches
   double cameraAngle = 15; //mounting angle
@@ -36,6 +37,7 @@ public class Limelight extends SubsystemBase {
     ta = table.getEntry("ta");
     thor = table.getEntry("thor");
     tvert = table.getEntry("tvert");
+    tid = table.getEntry("tid");
     botpose = table.getEntry("targetpose_robotspace");
     SmartDashboard.putNumber("ExpectedBotposeX", 0);
     SmartDashboard.putNumber("ExpectedBotposeY", 0);
@@ -45,28 +47,37 @@ public class Limelight extends SubsystemBase {
   public void periodic() {
     double forwardDistance = (targetHeight - cameraHeight) / Math.tan(Math.toRadians(cameraAngle + ty.getNumber(0.0).doubleValue()));
     double sidewaysDistance = forwardDistance * Math.tan(Math.toRadians(tx.getNumber(0.0).doubleValue())); //Middle of the camera to the middle of the tag
-    Number[] botposeArray = {0.0, 0.0};
+    Number[] botposeArray = {0.0, 0.0, 0.0};
     botposeArray = botpose.getNumberArray(botposeArray);
-    
+    double[] botposeArrayInches = new double[botposeArray.length];
+    for (int i = 0; i < botposeArray.length; i++) {
+      botposeArrayInches[i] = botposeArray[i].doubleValue();
+      botposeArrayInches[i] *= 39.36;
+    }
     SmartDashboard.putNumber("tv", tv.getNumber(0).doubleValue());
     SmartDashboard.putNumber("tx", tx.getNumber(0.0).doubleValue());
     SmartDashboard.putNumber("ty", ty.getNumber(0.0).doubleValue());
     SmartDashboard.putNumber("ta",  ta.getNumber(0.0).doubleValue());
     SmartDashboard.putNumber("thor", thor.getNumber(0.0).doubleValue());
     SmartDashboard.putNumber("tvert", tvert.getNumber(0.0).doubleValue());
+    SmartDashboard.putNumber("tid", tid.getNumber(0.0).doubleValue());
     SmartDashboard.putNumber("tforwardDistance", forwardDistance);
     SmartDashboard.putNumber("tsidewaysDistance", sidewaysDistance);
-    SmartDashboard.putNumber("botposeX", botposeArray[0].doubleValue());
-    SmartDashboard.putNumber("botposeY", botposeArray[1].doubleValue());
-    SmartDashboard.putNumber("botposeZ", botposeArray[2].doubleValue());
-    SmartDashboard.putNumber("botposeRoll", botposeArray[3].doubleValue());
-    SmartDashboard.putNumber("botposePitch", botposeArray[4].doubleValue());
-    SmartDashboard.putNumber("botposeYaw", botposeArray[5].doubleValue());
-    SmartDashboard.putNumber("DiferenceBotposeX", botposeArray[0].doubleValue() - SmartDashboard.getNumber("ExpectedBotposeZ", 0));
-    SmartDashboard.putNumber("DiferenceBotposeY", botposeArray[1].doubleValue() - SmartDashboard.getNumber("ExpectedBotposeY", 0));
-    SmartDashboard.putNumber("DiferenceBotposeZ", botposeArray[2].doubleValue() - SmartDashboard.getNumber("ExpectedBotposeZ", 0));
-    SmartDashboard.putNumber("MultiplierBotposeX",  botposeArray[0].doubleValue() / SmartDashboard.getNumber("ExpectedBotposeX", 0));
-    SmartDashboard.putNumber("MultiplierBotposeY", botposeArray[1].doubleValue() / SmartDashboard.getNumber("ExpectedBotposeY", 0));
-    SmartDashboard.putNumber("MultiplierBotposeZ", botposeArray[2].doubleValue() / SmartDashboard.getNumber("ExpectedBotposeZ", 0));
+
+    //Getting botpose data
+    SmartDashboard.putNumber("botposeX", botposeArrayInches[0]);
+    SmartDashboard.putNumber("botposeY", botposeArrayInches[1]);
+    SmartDashboard.putNumber("botposeZ", botposeArrayInches[2]);
+    SmartDashboard.putNumber("botposeRoll", botposeArrayInches[3]);
+    SmartDashboard.putNumber("botposePitch", botposeArrayInches[4]);
+    SmartDashboard.putNumber("botposeYaw", botposeArrayInches[5]);
+
+    //Checking why the data hates me
+    SmartDashboard.putNumber("OffsetBotposeX", Math.abs(botposeArrayInches[0]) - Math.abs(SmartDashboard.getNumber("ExpectedBotposeX", 0)));
+    SmartDashboard.putNumber("OffsetBotposeY", Math.abs(botposeArrayInches[1]) - Math.abs(SmartDashboard.getNumber("ExpectedBotposeY", 0)));
+    SmartDashboard.putNumber("OffsetBotposeZ", Math.abs(botposeArrayInches[2]) - Math.abs(SmartDashboard.getNumber("ExpectedBotposeZ", 0)));
+    SmartDashboard.putNumber("MultiplierBotposeX",  (botposeArrayInches[0] / SmartDashboard.getNumber("ExpectedBotposeX", 0)));
+    SmartDashboard.putNumber("MultiplierBotposeY", (botposeArrayInches[1] / SmartDashboard.getNumber("ExpectedBotposeY", 0)));
+    SmartDashboard.putNumber("MultiplierBotposeZ", (botposeArrayInches[2] / SmartDashboard.getNumber("ExpectedBotposeZ", 0)));
   }
 }
